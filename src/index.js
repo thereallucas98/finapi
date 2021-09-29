d31efc6bbb94df2b192641e789fa90e3b527ebb8
thereallucas98 <lucas1998david@gmail.com>
2021-09-29 19:48:27 -0300

    Validando CPF existente

--- src/index.js
+++ src/index.js
@@ -17,12 +17,16 @@ const customers = [];
 app.post("/account", (request, response) => {
   const { name, cpf } = request.body;
   
-  const id = uuidv4();
+  const customerAlreadyExists = customers.some((customer) => customer.cpf === cpf);
+
+  if (customerAlreadyExists) {
+    return response.status(400).json({ error: "Customer already Exists!"});
+  }
 
   customers.push({
     name,
     cpf,
-    id,
+    id: uuidv4(),
     statement: []
   });
 
